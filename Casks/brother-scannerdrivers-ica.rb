e511662d03598f10d06bb8a0dd7889288f7ec011
Adam Brown <acourtneybrown@gmail.com>
2023-08-01 15:56:56 -0700

    Initial cask file for `brother-scannerdrivers-ica`

---
+++ Casks/brother-scannerdrivers-ica.rb
@@ -0,0 +1,11 @@
+cask "brother-scannerdrivers-ica" do
+  version "1.5.0"
+  sha256 "6a9ef2865964ff4918dd1441876a80485fb8c329df36aa8d6680deff2eecd53e"
+
+  url "https://download.brother.com/welcome/dlf104746/Brother_ScannerDrivers_ICA_1_5_0.dmg"
+  name "Brother_ScannerDrivers_ICA"
+  desc "Scanner driver for Brother all-in-one printers"
+  homepage "https://support.brother.com/g/b/downloadhowto.aspx"
+
+  app ""
+end
