bceed6345f7621b6fe4c5ccdb354e5d30f6c57fe
Adam Brown <acourtneybrown@gmail.com>
2023-08-01 19:11:59 -0700

    Use `version.dots_to_underscores`

--- Casks/brother-scannerdrivers-ica.rb
+++ Casks/brother-scannerdrivers-ica.rb
@@ -1,9 +1,9 @@
 cask "brother-scannerdrivers-ica" do
   version "1.5.0"
-  # sha256 "6a9ef2865964ff4918dd1441876a80485fb8c329df36aa8d6680deff2eecd53e"
-  sha256 :no_check
+  sha256 "6a9ef2865964ff4918dd1441876a80485fb8c329df36aa8d6680deff2eecd53e"
+  # sha256 :no_check
 
-  url "https://download.brother.com/welcome/dlf104746/Brother_ScannerDrivers_ICA_1_5_0.dmg"
+  url "https://download.brother.com/welcome/dlf104746/Brother_ScannerDrivers_ICA_#{version.dots_to_underscores}.dmg"
   name "Brother_ScannerDrivers_ICA"
   desc "Scanner driver for Brother all-in-one printers"
   homepage "https://support.brother.com/g/b/downloadtop.aspx?c=us&lang=en&prod=mfc9970cdw_all"
