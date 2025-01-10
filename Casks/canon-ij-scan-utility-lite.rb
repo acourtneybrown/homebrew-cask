7edd8ff021aebac9b983f9bcca8cdec45bf047cc
Adam Brown <acourtneybrown@gmail.com>
2025-01-10 15:08:59 -0700

    Add cask for Canon's IJ Scan Utility Lite

---
+++ Casks/canon-ij-scan-utility-lite.rb
@@ -0,0 +1,15 @@
+cask "canon-ij-scan-utility-lite" do
+  version "4.1.2"
+  sha256 "d03a1b62379ead0fe7bdade28b8b817c7db4afd773ed37e6cb172e389e32791c"
+
+  url "https://gdlp01.c-wss.com/gds/7/0200005317/16/msul-mac-#{version.dots_to_underscores}-ea21_3.dmg"
+  name "IJ Scan Utility Lite"
+  desc "IJ Scan Utility Lite is the application software which enables you to scan photos and documents using AirPrint. You can easily scan such items simply by clicking the icon you want to choose in the main screen of IJ Scan Utility Lite."
+  homepage "https://www.csai.canon.com/support/p/canoscan-lide-400"
+
+  pkg "IJ Scan Utility Lite_040102.pkg"
+
+  uninstall pkgutil: ["jp.co.canon.pkg.ijscanutilityLite.040102"]
+
+  zap trash: "/Library/Application Support/Canon"
+end
