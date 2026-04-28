99ade4f1e4c62b6a707c4761ffadaa3d664c2923
Adam Brown <acourtneybrown@users.noreply.github.com>
2026-04-28 16:12:01 -0700

    Update canon-ij-scan-utility-lite `version` & `sha256`

--- Casks/canon-ij-scan-utility-lite.rb
+++ Casks/canon-ij-scan-utility-lite.rb
@@ -1,6 +1,6 @@
 cask "canon-ij-scan-utility-lite" do
-  version "4.1.2"
-  sha256 "d03a1b62379ead0fe7bdade28b8b817c7db4afd773ed37e6cb172e389e32791c"
+  version "4.2.1"
+  sha256 "8e072643503e0e86e5fa046b4bcf622fa33dfb7c701b030989e6616b16593bfa"
 
   url "https://gdlp01.c-wss.com/gds/7/0200005317/18/msul-mac-#{version.dots_to_underscores}-ea21_3.dmg",
       verified: "gdlp01.c-wss.com/gds/7/0200005317/18/"
